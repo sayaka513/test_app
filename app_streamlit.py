de16f00f79a2f46bda879a323b5f5fbf3ad62e61
sayaka513 <160210603+sayaka513@users.noreply.github.com>
2024-05-24 15:58:53 +0900

    Add files via upload

--- app_streamlit.py
+++ app_streamlit.py
@@ -9,8 +9,12 @@ model = YOLO("best_v8_25.pt")  # Path to the pre-trained YOLOv5 nano model
 st.title("Real-Time Object Detection with YOLOv8")
 
 
+
 # Define a function to draw bounding boxes with a specific color
 def draw_bounding_boxes(img, results):
+     # Initialize counters
+    helmet_count = 0
+    no_helmet_count = 0
     for result in results:
         for box in result.boxes:
             bbox = box.xyxy[0].cpu().numpy().astype(int)  # Get the bounding box coordinates
@@ -21,13 +25,19 @@ def draw_bounding_boxes(img, results):
             if class_id == 1:
                 color = (0, 0, 255)  # Red for no helmet
                 label = f"No Helmet: {confidence:.2f}"
+                no_helmet_count += 1
             else:
                 color = (0, 255, 0)  # Green for helmet
                 label = f"Helmet: {confidence:.2f}"
+                helmet_count += 1
 
             # Draw the bounding box and label on the image
             cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
             cv2.putText(img, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+            # Display the counts on the frame
+            cv2.putText(img, f"Helmet: {helmet_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, f"No Helmet: {no_helmet_count}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     return img
 
